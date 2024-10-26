58eded3980f5b337b3c7403482e3c9dcf68ce2ae
Mostafam12 <mostafa71@hotmail.co.uk>
2024-10-26 16:40:10 -0600

    OnGameStart set up. BuildOrderItem struct built

--- BasicSc2Bot.h
+++ BasicSc2Bot.h
@@ -6,6 +6,7 @@
 #include "sc2lib/sc2_lib.h"
 #include "sc2utils/sc2_manage_process.h"
 #include "sc2utils/sc2_arg_parser.h"
+#include <vector>
 
 class BasicSc2Bot : public sc2::Agent {
 public:
@@ -13,6 +14,26 @@ public:
 	virtual void OnStep();
 
 private:
+	// Temporary struct to hold build order
+    struct BuildOrderItem {
+        int supply;                   // Supply count at which to build the gien unit/ability (0 if ASAP)
+        sc2::UNIT_TYPEID unit_type;   // Unit or structure to build
+        sc2::ABILITY_ID ability;      // Ability to use (for upgrades or morphs)
+        bool is_unit;                 // True if unit_type is valid, false if ability is valid
+
+        // Constructor for units and structures
+        BuildOrderItem(int s, sc2::UNIT_TYPEID u)
+            : supply(s), unit_type(u), ability(sc2::ABILITY_ID::INVALID), is_unit(true) {}
+
+        // Constructor for abilities
+        BuildOrderItem(int s, sc2::ABILITY_ID a)
+            : supply(s), unit_type(sc2::UNIT_TYPEID::INVALID), ability(a), is_unit(false) {}
+    };
+	size_t build_order_index; // What item in the build_order we're currently on
+    std::vector<BuildOrderItem> build_order; // Vector that holds BuildOrderItems
+    std::vector<sc2::Point3D> scout_locations; // Vector containing locations we need to scout
+    std::vector<sc2::Point3D> enemy_bases; // Vector containing locations we identify as enemy bases
+
 };
 
 #endif
