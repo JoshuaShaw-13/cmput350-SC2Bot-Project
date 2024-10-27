97eefdb46a7658a13fb6dbe22c357c069c27ff7f
meghan <144631876+meghanwickstrand@users.noreply.github.com>
2024-10-27 16:41:03 -0600

    BuildQueueu.cpp and AttackTargetQueue.cpp implementations (incomplete)

--- classes/BuildQueue.h
+++ classes/BuildQueue.h
@@ -3,6 +3,11 @@
  */
 
 #include <queue>
+#include "sc2api/sc2_api.h"
+#include "sc2api/sc2_args.h"
+#include "sc2lib/sc2_lib.h"
+#include "sc2utils/sc2_manage_process.h"
+#include "sc2utils/sc2_arg_parser.h"
 
 class BuildQueue{
     public:
@@ -26,4 +31,18 @@ class BuildQueue{
 
 };
 
-struct Unit{/** struct for holding Unit information */};
+struct Unit{
+    /** struct for holding Unit information */
+    int supply;                   // Supply count at which to build the gien unit/ability (0 if ASAP)
+    sc2::UNIT_TYPEID unit_type;   // Unit or structure to build
+    sc2::ABILITY_ID ability;      // Ability to use (for upgrades or morphs)
+    bool is_unit;                 // True if unit_type is valid, false if ability is valid
+
+    // Constructor for units and structures
+    BuildOrderItem(int s, sc2::UNIT_TYPEID u)
+        : supply(s), unit_type(u), ability(sc2::ABILITY_ID::INVALID), is_unit(true) {}
+
+    // Constructor for abilities
+    BuildOrderItem(int s, sc2::ABILITY_ID a)
+        : supply(s), unit_type(sc2::UNIT_TYPEID::INVALID), ability(a), is_unit(false) {}
+    };
