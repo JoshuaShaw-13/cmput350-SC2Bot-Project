23cba6b01799b72b652d670a269ea3cc79ae1927
meghan <144631876+meghanwickstrand@users.noreply.github.com>
2024-10-27 18:55:02 -0600

    fixed BuildQueue.cpp

--- classes/BuildQueue.h
+++ classes/BuildQueue.h
@@ -19,34 +19,34 @@ class BuildQueue{
         BuildQueue &operator=(const BuildQueue &q){};//assignment operator
 
         //getter(s)
-        std::queue<Unit*>* getQueue() const{}
+        std::queue<BuildOrderItem*>* getQueue() const{}
 
         //methods
         bool isEmpty() const{/** returns true if the queue is empty */ }
-        Unit* peek() const{/** returns a pointer to the next building in the queue */}
-        Unit* pop() {/** removes the next building in the queue, and returns a pointer to that building */};
-        void push(const Unit &unit){/**  adds the given Unit to the queue*/}
+        BuildOrderItem* peek() const{/** returns a pointer to the next building in the queue */}
+        BuildOrderItem* pop() {/** removes the next building in the queue, and returns a pointer to that building */};
+        void push(const BuildOrderItem &BuildOrderItem){/**  adds the given BuildOrderItem to the queue*/}
 
     private:
     //data
-    std::queue<Unit*>* data;
+    std::queue<BuildOrderItem*>* data;
 
 };
 
-struct Unit{
-    /** struct for holding Unit information */
-    int supply;                   // Supply count at which to build the gien unit/ability (0 if ASAP)
-    sc2::UNIT_TYPEID unit_type;   // Unit or structure to build
+struct BuildOrderItem{
+    /** struct for holding BuildOrderItem information */
+    int supply;                   // Supply count at which to build the gien BuildOrderItem/ability (0 if ASAP)
+    sc2::BuildOrderItem_TYPEID BuildOrderItem_type;   // BuildOrderItem or structure to build
     sc2::ABILITY_ID ability;      // Ability to use (for upgrades or morphs)
-    bool is_unit;                 // True if unit_type is valid, false if ability is valid
+    bool is_BuildOrderItem;                 // True if BuildOrderItem_type is valid, false if ability is valid
 
-    // Constructor for units and structures
-    BuildOrderItem(int s, sc2::UNIT_TYPEID u)
-        : supply(s), unit_type(u), ability(sc2::ABILITY_ID::INVALID), is_unit(true) {}
+    // Constructor for BuildOrderItems and structures
+    BuildOrderItem(int s, sc2::BuildOrderItem_TYPEID u)
+        : supply(s), BuildOrderItem_type(u), ability(sc2::ABILITY_ID::INVALID), is_BuildOrderItem(true) {}
 
     // Constructor for abilities
     BuildOrderItem(int s, sc2::ABILITY_ID a)
-        : supply(s), unit_type(sc2::UNIT_TYPEID::INVALID), ability(a), is_unit(false) {}
+        : supply(s), BuildOrderItem_type(sc2::BuildOrderItem_TYPEID::INVALID), ability(a), is_BuildOrderItem(false) {}
     };
 
 #endif
