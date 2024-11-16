e74d36cf088efbaee9ac14437a4b460bd5110b7b
Joshua-Shaw <141381258+JoshuaShaw-13@users.noreply.github.com>
2024-11-16 16:13:45 -0700

    fix overlord scouting and rallying

--- BasicSc2Bot.h
+++ BasicSc2Bot.h
@@ -1,18 +1,17 @@
 #ifndef BASIC_SC2_BOT_H_
 #define BASIC_SC2_BOT_H_
 
-
+#include "AttackBaseQueue.h"
+#include "BuildQueue.h"
 #include "sc2api/sc2_api.h"
 #include "sc2api/sc2_args.h"
 #include "sc2lib/sc2_lib.h"
-#include "sc2utils/sc2_manage_process.h"
 #include "sc2utils/sc2_arg_parser.h"
-#include <vector>
-#include <set>
-#include "AttackBaseQueue.h"
-#include "BuildQueue.h"
+#include "sc2utils/sc2_manage_process.h"
 #include <sc2api/sc2_agent.h>
 #include <sc2api/sc2_unit_filters.h>
+#include <set>
+#include <vector>
 
 using namespace sc2;
 
@@ -24,23 +23,26 @@ public:
   virtual void OnUnitIdle(const Unit *unit) final;
 
 private:
-  const Unit *FindNearestMineralPatch(const Point2D&);
-  const Unit *FindNearestVespenePatch(const Point2D&);
+  const Unit *FindNearestMineralPatch(const Point2D &);
+  const Unit *FindNearestVespenePatch(const Point2D &);
   const Unit *findIdleLarva();
   const Unit *findIdleDrone();
-  const Unit* findAvailableDrone();
+  const Unit *findAvailableDrone();
   const Unit *findAvailableLarva();
-  Point2D findBuildPositionNearMineral(const Point2D&);
-  Point2D findBuildPosition(const Point2D&);
+  Point2D findBuildPositionNearMineral(const Point2D &);
+  Point2D findBuildPosition(const Point2D &);
   bool tryBuild(struct BuildOrderItem);
   bool isArmyReady();
-  void launchAttack( const Point2D&);
+  bool inRallyRange(const Point2D &, const Point2D &, float);
+  void launchAttack(const Point2D &);
   BuildQueue build_order; // Queue that holds BuildOrderItems
-  std::vector<Point2D> scout_locations; // Vector containing locations we need to scout
-  std::set<const Unit*> mineral_locations;
-  std::set<const Unit*> vespene_locations;
+  std::vector<Point2D>
+      scout_locations; // Vector containing locations we need to scout
+  std::set<const Unit *> mineral_locations;
+  std::set<const Unit *> vespene_locations;
   bool launching_attack = false;
-  AttackBaseQueue enemy_bases; // Queue containing locations we identify as enemy bases
+  AttackBaseQueue
+      enemy_bases; // Queue containing locations we identify as enemy bases
 };
 
 #endif
