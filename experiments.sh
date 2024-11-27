7120b814d5271aad842590e58c1a740133316a96
Joshua-Shaw <141381258+JoshuaShaw-13@users.noreply.github.com>
2024-11-27 16:54:30 -0700

    add comments

--- experiments.sh
+++ experiments.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# go to path holding BasicSc2Bot.exe
 cd build/bin/
 
+# Things to parameterize
+#  - Enemy Race (Terran, Zerg, Protoss)
+#  - Enemy Difficulty (Beginner, Easy, Moderate, Hard, Insane)
+#  - Map (CactusValleyLE.SC2Map, BelShirVestigeLE.SC2Map, and ProximaStationLE.SC2Map)
+#  - Number of Roaches in army (???)
+#  - Number of Zerglings in army (???)
+
+# execute BasicSc2Bot.exe
 ./BasicSc2Bot.exe -c -a zerg -d Hard -m CactusValleyLE.SC2Map
+
+# Collect and log results (our win or enemy win)
