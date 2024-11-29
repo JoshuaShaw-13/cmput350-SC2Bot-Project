95b3f822d31d3a8eb0ed2c1ead501830323762ed
Joshua-Shaw <141381258+JoshuaShaw-13@users.noreply.github.com>
2024-11-29 16:34:32 -0700

    add roach amount for loop

--- experiments.sh
+++ experiments.sh
@@ -7,16 +7,19 @@ cd build/bin/
 #  - Enemy Race (Terran, Zerg, Protoss)
 #  - Enemy Difficulty (Beginner, Easy, Moderate, Hard, Insane)
 #  - Map (CactusValleyLE.SC2Map, BelShirVestigeLE.SC2Map, and ProximaStationLE.SC2Map)
-#  - Number of Roaches in army (???)
-#  - Number of Zerglings in army (???)
+#  - Number of Roaches in army (3 - 12)
 
 # execute BasicSc2Bot.exe
 for race in zerg terran protoss; do
-    for diff in Beginner Easy Moderate Hard Insane; do
+    for diff in Easy Moderate Hard; do #additional options Beginner, Insane
         for map in CactusValleyLE.SC2Map BelShirVestigeLE.SC2Map ProximaStationLE.SC2Map; do
-            ./BasicSc2Bot.exe -c -a $race -d $diff -m $map
+            for r_amount in 3 6 9 12; do
+                ./BasicSc2Bot.exe -c -a $race -d $diff -m $map
+            done
         done
     done
 done
+
+#./BasicSc2Bot.exe -c -a zerg -d Hard -m CactusValleyLE.SC2Map > match_output.txt
 
 # Collect and log results (our win or enemy win)
