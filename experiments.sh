3f7ba2cf6a1d7ea0e33b7005273c149bfb0019f6
Joshua-Shaw <141381258+JoshuaShaw-13@users.noreply.github.com>
2024-11-29 17:34:01 -0700

    add logging match results

--- experiments.sh
+++ experiments.sh
@@ -14,7 +14,24 @@ for race in zerg terran protoss; do
     for diff in Easy Moderate Hard; do #additional options Beginner, Insane
         for map in CactusValleyLE.SC2Map BelShirVestigeLE.SC2Map ProximaStationLE.SC2Map; do
             for r_amount in 3 6 9 12; do
-                ./BasicSc2Bot.exe -c -a $race -d $diff -m $map
+                win_count = 0
+                loss_count = 0
+                tie_count = 0
+                echo "$race, $diff, $map, $r_amount: " >> bot_results.txt
+                for run in 1 2 3 4 5; do
+                    ./BasicSc2Bot.exe -c -a $race -d $diff -m $map > match_output.txt
+                    if grep -q "Victory" match_output.txt; then
+                        let win_count+=1
+                    elif grep -q "Defeat" match_output.txt; then
+                        let loss_count+=1
+                    elif grep -q "Tie" match_output.txt; then
+                        let tie_count+=1
+                    fi
+                done
+                echo "Wins: $win_count" >> bot_results.txt
+                echo "Losses: $loss_count" >> bot_results.txt
+                echo "Ties: $tie_count" >> bot_results.txt
+                echo "" >> bot_results.txt
             done
         done
     done
