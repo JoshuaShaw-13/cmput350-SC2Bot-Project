be41435041b1052ed0ca5e36e1a06dbc6153d099
Joshua-Shaw <141381258+JoshuaShaw-13@users.noreply.github.com>
2024-11-30 11:20:32 -0700

    change file location of results

--- experiments.sh
+++ experiments.sh
@@ -3,7 +3,7 @@
 # go to path holding BasicSc2Bot.exe
 cd build/bin/
 
-# Things to parameterize
+# Things to test
 #  - Enemy Race (terran, zerg, protoss)
 #  - Enemy Difficulty (Beginner, Easy, Moderate, Hard, Insane)
 #  - Map (CactusValleyLE.SC2Map, BelShirVestigeLE.SC2Map, ProximaStationLE.SC2Map)
@@ -19,27 +19,23 @@ for race in zerg terran protoss; do
                     win_count=0
                     loss_count=0
                     tie_count=0
-                    echo "$race, $diff, $map, $r_amount, $dr_amount: " >> bot_results.txt
+                    echo "$race, $diff, $map, $r_amount, $dr_amount: " >> ../../bot_results.txt
                     for run in 1 2 3 4 5; do
-                        ./BasicSc2Bot.exe -c -a $race -d $diff -m $map -r $r_amount -dr $dr_amount > match_output.txt
-                        if grep -q "Victory" match_output.txt; then
+                        ./BasicSc2Bot.exe -c -a $race -d $diff -m $map -r $r_amount -dr $dr_amount > ../../match_output.txt
+                        if grep -q "Victory" ../../match_output.txt; then
                             let win_count+=1
-                        elif grep -q "Defeat" match_output.txt; then
+                        elif grep -q "Defeat" ../../match_output.txt; then
                             let loss_count+=1
-                        elif grep -q "Tie" match_output.txt; then
+                        elif grep -q "Tie" ../../match_output.txt; then
                             let tie_count+=1
                         fi
                     done
-                    echo "Wins: $win_count" >> bot_results.txt
-                    echo "Losses: $loss_count" >> bot_results.txt
-                    echo "Ties: $tie_count" >> bot_results.txt
-                    echo "" >> bot_results.txt
+                    echo "Wins: $win_count" >> ../../bot_results.txt
+                    echo "Losses: $loss_count" >> ../../bot_results.txt
+                    echo "Ties: $tie_count" >> ../../bot_results.txt
+                    echo "" >> ../../bot_results.txt
                 done
             done
         done
     done
 done
-
-#./BasicSc2Bot.exe -c -a zerg -d Hard -m CactusValleyLE.SC2Map > match_output.txt
-
-# Collect and log results (our win or enemy win)
